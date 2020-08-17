302f717434aea56999479c1d7c75d9cf67fc4fc9
Sharan <michinaux.marko@gmail.com>
2020-08-16 21:41:01 -0400

    merging to master (#1)
    * Initial commit with Gomix
    
    * fix cors domain for beta and freecodecamp.org (#1)
    
    add `https://www.freecodecamp.org` and `https://beta.freecodecamp.org` to the CORS enabled domains to access the boilerplate. 
    
    Closes freeCodeCamp/freeCodeCamp#16915
    
    * Fix create new middleware test #2
    
    Co-authored-by: System <none>
    Co-authored-by: Emanuele Antonelli <joshua999x@gmail.com>
    Co-authored-by: Skip Hendriks <sigmar001@gmail.com>
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-"use strict";
+'use strict';
 
-const express = require("express");
-const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
-app.use("/public", express.static(process.cwd() + "/public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-app.route("/").get((req, res) => {
-  //Change the response to render the Pug template
-  res.send(`Pug template is not defined.`);
-});
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.route('/')
+  .get((req, res) => {
+    res.sendFile(process.cwd() + '/views/index.html');
+  });
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + process.env.PORT);
