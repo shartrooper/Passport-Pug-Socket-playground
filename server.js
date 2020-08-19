e9fc8481d95a7e8f962b38d2896e0c5c94d98377
Glitch (sharkantropo-passport-pugsocket) <none>
2020-08-19 15:46:04 +0000

    🐡🎖 Updated with Glitch

--- server.js
+++ server.js
@@ -1,27 +1,45 @@
-'use strict';
+"use strict";
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const pug = require('pug');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const routes = require("./routes.js");
+const auth = require("./auth.js");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const session = require("express-session");
+const pug = require("pug");
 const app = express();
+const mongo = require("mongodb").MongoClient;
 
 fccTesting(app); //For FCC testing purposes
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 app.set("views", __dirname + "/views/pug");
 
-app.route('/')
-  .get((req, res) => {
-    res.render('index');
-  });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
-});
+mongo.connect(
+  process.env.DATABASE,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, client) => {
+    var db = client.db("fcc_test_users");
+    if (err) {
+      console.log("Database error: " + err);
+    } else {
+      console.log("Successful database connection");
+
+      auth(app, db);
+
+      routes(app, db);
+
+      app.listen(process.env.PORT || 3000, () => {
+        console.log("Listening on port " + process.env.PORT);
+      });
+    }
+  }
+);
+
+
+
+
